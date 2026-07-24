e31bf76c7790bd8639fb06d7b6bb35f9e92c74cc
ezbuildds <ezbuildds@gmail.com>
2026-07-24 11:18:39 +0530

    create failed api

--- server/controller/failedPayment.js
+++ server/controller/failedPayment.js
@@ -1,9 +1,35 @@
+import { Long } from "mongodb";
+import dbConnection from "../config/db.js";
+
 export default async function failedPayment(req, res) {
-    const { razorpayOrderId, error } = req.body
-    console.log("ID :", razorpayOrderId);
-    console.log("Error :", error);
-    return res.status(200).send({
-        success: true,
-        message: "Payment failed processed successfully."
-    })
+    try {
+        const { razorpayOrderId, error } = req.body
+        console.log("ID :", razorpayOrderId);
+        console.log("Error :", error);
+        if (!razorpayOrderId || !error) {
+            return res.status(400).send({
+                success: false,
+                message: "Invalid request."
+            })
+        }
+        const db = await dbConnection()
+        await db.collection("transaction").updateOne({ razorpayOrderId }, {
+            $set: {
+                status: "failed",
+                updateAt: new Date()
+            }
+        })
+        return res.status(200).send({
+            success: false,
+            message: "Payment failed."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: false,
+            message: "Internal server error"
+        })
+
+    }
+
 }
